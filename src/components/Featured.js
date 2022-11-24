4ee3ab9dd1dc5e7f7a9cb1738cf9976af9285cab
kcdodson <kaceydodson+github@gmail.com>
2022-11-24 16:29:40 -0500

    initial commit

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,12 +1,12 @@
 import React from "react";
-import picture2 from "../photos/lmfao.jpg";
+import { Link, Outlet } from "react-router-dom";
 import picture1 from "../photos/background2.jpg";
-import Card from "./Card";
 import bg1 from "../photos/bg01.jpg";
 import bg2 from "../photos/bg02.jpg";
 import bg3 from "../photos/bg03.jpg";
 import bg4 from "../photos/bg04.jpg";
-import { Outlet, Link } from "react-router-dom";
+import picture2 from "../photos/lmfao.jpg";
+import Card from "./Card";
 
 const Featured = () => {
   const cards = [
@@ -94,7 +94,7 @@ const Featured = () => {
       </div>
 
       {/* Cards*/}
-      <div className="grid grid-cols-1 xs:grid-cols-2   lg:grid-cols-3 gap-y-8 xs:gap-x-2 sm:gap-x-4 mb-4  ">
+      <div className="grid grid-cols-1 xs:grid-cols-2 2xl:grid-cols-4 lg:grid-cols-3 gap-y-8 xs:gap-x-2 sm:gap-x-4 mb-4">
         {cards.map((card) => (
           <Link to="/card">
             <Card
